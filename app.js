5805c479074dcbb95413f4a707c1dd424cf239ac
RahmaHassan <rahmaashraf053@gmail.com>
2024-01-28 23:30:19 +0300

    'feat: Implement randomized placement for food'

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const playBoardHeight = playBoard.offsetHeight;
 let foodX, foodY, snakeX=20, snakeY=20;/*when we create function to change the posisiton of the snake we will remove 20 */
 
 const changeFoodPosition = () => {
-    foodX = Math.floor(Math.random() * (playBoardWidth / 30)) + 2;// we used 30 here cuz is CSS we specified the num of rows&cols with 30
-    foodY = Math.floor(Math.random() * (playBoardHeight / 30)) + 2; 
+    foodY = Math.floor(Math.random() * (playBoardWidth / 30)) + 2;// we used 30 here cuz is CSS we specified the num of rows&cols with 30
+    foodX = Math.floor(Math.random() * (playBoardHeight / 30)) + 2; 
   };
 const initGame = () => {
   
